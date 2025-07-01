80214c540df35f9fec465cc9c7ce0f9dc3d21dc2
jiseok08 <heoji0912@gmail.com>
2025-07-01 21:44:30 +0900

    (2025.7.1) 선형 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,44 +1,54 @@
 ﻿#include <iostream>
-#include "Packit.h"
-#include "Resource.h"
+#include <vector>
+#include <list>
 
 using namespace std;
 
 int main()
 {
-#pragma region 스마트 포인터
-	// 포인터를 사용하는 동시에 자동으로 메모리 관리를
-	// 해주며, 경계 확인과 같은 추가 기능을 제공하는 포인터입니다.
+#pragma region 선형 컨테이너
+	// 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 컨테이너입니다.
 
-#pragma region unique pointer
-	// 특정한 객체를 하나의 스마트 포인터만 가리킬 수
-	// 있도록 되어 있는 포인터입니다.
+#pragma region vector container
 
-	// unique_ptr<Packit> uniquePointer = make_unique<Packit>();
+	// std::vector<int> vector;
 	// 
-	// cout << "Error Code : " << uniquePointer->Error() << endl;
+	// vector.reserve(8);
 	// 
-	// unique_ptr<Packit> reference = std::move(uniquePointer);
+	// vector.push_back(10);
+	// vector.push_back(20);
+	// vector.push_back(30);
+	// vector.push_back(40);
+	// vector.push_back(50);
 	// 
-	// cout << "Error Code : " << reference->Error() << endl;
+	// 
+	// for (int i = 0; i < vector.size(); i++)
+	// {
+	// 	cout << vector[i] << endl;
+	// }
+	// 
+	// vector.clear();
+	// 
+	// cout << "Vector의 Size의 값 : " << vector.size() << endl;
+	// cout << "Vector의 Capacity의 값 : " << vector.capacity() << endl;
+	// 
+	// cout << vector.capacity() << endl;
 
 #pragma endregion
 
-#pragma region shared pointer
-	// 하나의 자원 객체를 여러 포인터 객체가 가리킬 수 있으며,
-	// 모든 포인터 객체가 자원 객체를 필요로 하지 않을 때 
-	// 자원 객체를 해제하도록 설계되어 있는 포인터입니다.
+#pragma region list container
 
-	// shared_ptr<Resource> oil = make_shared<Resource>();
-	// 
-	// shared_ptr<Resource> mineral = make_shared<Resource>();
-	// 
-	// Resource resource;
-	// 
-	// oil->Share(mineral);
-	// mineral->Share(oil);
-	// 
-	// cout << "oil Reference count : " << oil.use_count() << endl;
+	std::list<int> list;
+	list.push_back(20);
+	list.push_back(30);
+	list.push_front(10);
+
+	list.pop_front();
+
+	for (int data : list)
+	{
+		cout << data << ' ';
+	}
 
 #pragma endregion
 
