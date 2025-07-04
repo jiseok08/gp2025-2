ab8ed9993033222fdabb88a441a01a5937c02fca
jiseok08 <heoji0912@gmail.com>
2025-07-04 21:43:25 +0900

    (2025.7.4) 반복자

---
+++ Class12th (Container Adapter)/Source.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+#pragma region 컨테이너 어댑터
+	// 기존 컨테이너의 인터페이스를 제한하여 만든 기능이
+	// 제한되거나 변경된 컨테이너입니다.
+
+#pragma region stack container
+
+	// std::stack<int> stack;
+	// 
+	// stack.push(10);
+	// stack.push(20);
+	// stack.push(30);
+	// stack.push(40);
+	// stack.push(50);
+	// 
+	// while (stack.empty() == false)
+	// {
+	// 	cout << stack.top() << endl;
+	// 
+	// 	stack.pop();
+	// }
+
+#pragma endregion
+
+#pragma region queue container
+
+	// std::queue<int> queue;
+	// 
+	// queue.push(10);
+	// queue.push(20);
+	// queue.push(30);
+	// queue.push(40);
+	// queue.push(50);
+	// 
+	// while (queue.empty() == false)
+	// {
+	// 	cout << queue.front() << endl;
+	// 
+	// 	queue.pop();
+	// }
+
+#pragma endregion
+
+#pragma region priority queue
+
+	// std::priority_queue<int> priorityQueue;
+	// 
+	// priorityQueue.push(10);
+	// priorityQueue.push(20);
+	// priorityQueue.push(9);
+	// priorityQueue.push(31);
+	// 
+	// while(priorityQueue.empty() == false)
+	// {
+	// 	cout << priorityQueue.top() << endl;
+	// 
+	// 	priorityQueue.pop();
+	// }
+
+#pragma endregion
+
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <unordered_set>
+#include <unordered_map>
+#include <string>
 
 using namespace std;
 
@@ -7,31 +9,79 @@ int main()
 {
 #pragma region unordered set
 
-	unordered_set<const char *> unordered_set;
-
-	unordered_set.max_load_factor(2.0);
-
-	unordered_set.insert("James");
-	unordered_set.insert("Harry");
-	unordered_set.insert("Bob");
-	unordered_set.insert("Bob1");
-	unordered_set.insert("Bob2");
-	unordered_set.insert("Bob3");
-	unordered_set.insert("Bob4");
-	unordered_set.insert("Bob5");
-	unordered_set.insert("Bob6");
-	unordered_set.insert("Bob7");
-	
-
-	cout << "Bucket Count : " << unordered_set.bucket_count() << endl;
-
-	for (const char * element : unordered_set)
-	{
-		cout << element << " ";
-	}
+	// unordered_set<const char *> unordered_set;
+	// 
+	// unordered_set.max_load_factor(2.0);
+	// 
+	// unordered_set.insert("James");
+	// unordered_set.insert("Harry");
+	// unordered_set.insert("Bob");
+	// unordered_set.insert("Bob1");
+	// unordered_set.insert("Bob2");
+	// unordered_set.insert("Bob3");
+	// unordered_set.insert("Bob4");
+	// unordered_set.insert("Bob5");
+	// unordered_set.insert("Bob6");
+	// unordered_set.insert("Bob7");
+	// 
+	// cout << "Load Factor : " << unordered_set.load_factor() << endl;
+	// cout << "Bucket Count : " << unordered_set.bucket_count() << endl;
+	// 
+	// cout << endl;
+	// 
+	// if (unordered_set.find("Bob") != unordered_set.end())
+	// {
+	// 	cout << "the data exists..." << endl;
+	// }
+	// else 
+	// {
+	// 	cout << "the data does not exist..." << endl;
+	// }
+	// 
+	// unordered_set.erase("Bob");
+	// 
+	// for (const char* element : unordered_set)
+	// {
+	// 	cout << element << " ";
+	// }
 
 #pragma endregion
 
+#pragma region unordered map
+
+	// std::unordered_map<const char *, int> unordered_map;
+	// 
+	// unordered_map.insert({ "Infinity Edge", 2900 });
+	// unordered_map.insert({ "Essence Reaver", 3100 });
+	// unordered_map.insert({ "Bloodthirster", 3333 });
+	// unordered_map.insert({ "Kraken Slayer", 3300 });
+	// unordered_map.insert({ "Rapid Firecannon", 3400 });
+	// unordered_map.insert({ "Thornmail", 3400 });
+	// 
+	// cout << "Load Factor : " << unordered_map.load_factor() << endl;
+	// 
+	// cout << "unordered_map size : " << unordered_map.size() << endl;
+	// 
+	// unordered_map.erase("Bloodthirster");
+	// 
+	// for (const auto & element : unordered_map)
+	// {
+	// 	cout << "Key : " << element.first << endl;
+	// 	cout << "Value : " << element.second << endl;
+	// 	cout << endl;
+	// }
+	// 
+	// unordered_map.clear();
+	// 
+	// cout << "Load Factor : " << unordered_map.load_factor() << endl;
+
+#pragma endregion
+
+	// 10 6 4 8 20
+
+	// 6 4 
+
+	// 6 4 8 20 10
 
 	return 0;
 }
